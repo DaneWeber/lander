d57b60d1b9b178a09f3b8e2a9ec68caa3909cdad
Dane Weber <DaneWeber@users.noreply.github.com>
2022-12-23 00:11:26 +0000

    Basic arrow key responsiveness

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,12 +1,24 @@
 import './Controls.css';
+import useKeyPress from './hooks/useKeyPress';
+import React, { useState } from 'react';
 
 function Controls() {
+  const [active, setActive] = useState(0);
+  const handleKeyDown = (event: { keyCode: React.SetStateAction<number>; preventDefault: () => void; }) => {
+    if (event.keyCode >= 37 && event.keyCode <= 40) {
+      event.preventDefault();
+      setActive(event.keyCode);
+    }
+  }
+
+  useKeyPress("keydown", handleKeyDown);
+
   return (
     <div className="Controls">
-      <div className="up">&uArr;</div>
-      <div className="left">&lArr;</div>
-      <div className="down">&dArr;</div>
-      <div className="right">&rArr;</div>
+      <div className={`up ${active === 38 ? "active" : ""}`}>&uArr;</div>
+      <div className={`left ${active === 37 ? "active" : ""}`}>&lArr;</div>
+      <div className={`down ${active === 40 ? "active" : ""}`}>&dArr;</div>
+      <div className={`right ${active === 39 ? "active" : ""}`}>&rArr;</div>
     </div>
   );
 }
