5056e3a3a88eec7edb3f08ab4b9a3eb4bd159671
Dane Weber <DaneWeber@users.noreply.github.com>
2022-12-23 00:58:32 +0000

    Move keyboard listener to the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Controls from './Controls';
 import logo from './logo.svg';
 import './App.css';
+import useKeyPress from './hooks/useKeyPress';
+
 
 function App() {
+  const [active, setActive] = useState({up: '', left: '', down: '', right: ''});
+  const handleKeyDown = (event: { keyCode: React.SetStateAction<number>; preventDefault: () => void; }) => {
+    if (event.keyCode >= 37 && event.keyCode <= 40) {
+      event.preventDefault();
+      let activeClone = Object.assign(Object.create(Object.getPrototypeOf(active)), active);
+      switch (event.keyCode) {
+        case 37: activeClone.left = 'active'; break;
+        case 38: activeClone.up = 'active'; break;
+        case 39: activeClone.right = 'active'; break;
+        case 40: activeClone.down = 'active'; break;
+      }
+      ['up', 'left', 'down', 'right'].forEach((key) => {
+        activeClone[key] = activeClone[key] === 'warm' ? '' : activeClone[key]
+      });
+
+      setActive(activeClone);
+    }
+  }
+
+  useKeyPress("keydown", handleKeyDown);
+
+  const handleKeyUp = (event: { keyCode: React.SetStateAction<number>; preventDefault: () => void; }) => {
+    if (event.keyCode >= 37 && event.keyCode <= 40) {
+      event.preventDefault();
+      let activeClone = Object.assign(Object.create(Object.getPrototypeOf(active)), active);
+      switch (event.keyCode) {
+        case 37: activeClone.left = 'warm'; break;
+        case 38: activeClone.up = 'warm'; break;
+        case 39: activeClone.right = 'warm'; break;
+        case 40: activeClone.down = 'warm'; break;
+      }
+
+      setActive(activeClone);
+    }
+  }
+
+  useKeyPress("keyup", handleKeyUp);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Lander</h1>
-        <Controls />
+        <Controls active={active} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,45 +1,7 @@
 import './Controls.css';
-import useKeyPress from './hooks/useKeyPress';
-import React, { useState } from 'react';
 
-function Controls() {
-  const [active, setActive] = useState({up: '', left: '', down: '', right: ''});
-  const handleKeyDown = (event: { keyCode: React.SetStateAction<number>; preventDefault: () => void; }) => {
-    if (event.keyCode >= 37 && event.keyCode <= 40) {
-      event.preventDefault();
-      let activeClone = Object.assign(Object.create(Object.getPrototypeOf(active)), active);
-      switch (event.keyCode) {
-        case 37: activeClone.left = 'active'; break;
-        case 38: activeClone.up = 'active'; break;
-        case 39: activeClone.right = 'active'; break;
-        case 40: activeClone.down = 'active'; break;
-      }
-      ['up', 'left', 'down', 'right'].forEach((key) => {
-        activeClone[key] = activeClone[key] === 'warm' ? '' : activeClone[key]
-      });
-
-      setActive(activeClone);
-    }
-  }
-
-  useKeyPress("keydown", handleKeyDown);
-
-  const handleKeyUp = (event: { keyCode: React.SetStateAction<number>; preventDefault: () => void; }) => {
-    if (event.keyCode >= 37 && event.keyCode <= 40) {
-      event.preventDefault();
-      let activeClone = Object.assign(Object.create(Object.getPrototypeOf(active)), active);
-      switch (event.keyCode) {
-        case 37: activeClone.left = 'warm'; break;
-        case 38: activeClone.up = 'warm'; break;
-        case 39: activeClone.right = 'warm'; break;
-        case 40: activeClone.down = 'warm'; break;
-      }
-
-      setActive(activeClone);
-    }
-  }
-
-  useKeyPress("keyup", handleKeyUp);
+function Controls(props: { active: any; }) {
+  const active = props.active;
 
   return (
     <div className="Controls">
