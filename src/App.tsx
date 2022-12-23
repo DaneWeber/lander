d12af59930d20dedab21da835c284e38bc598379
Dane Weber <DaneWeber@users.noreply.github.com>
2022-12-23 00:59:25 +0000

    Remove React logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Controls from './Controls';
-import logo from './logo.svg';
 import './App.css';
 import useKeyPress from './hooks/useKeyPress';
 
@@ -49,18 +48,6 @@ function App() {
       <header className="App-header">
         <h1>Lander</h1>
         <Controls active={active} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
